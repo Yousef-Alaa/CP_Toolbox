15a486416a898b072873c2ac2d3623c4533bee08
Yousef Alaa <yousefdev@proton.me>
2026-07-13 23:31:08 +0300

    add Hilbert curve order

--- Algorithms/MO.cpp
+++ Algorithms/MO.cpp
@@ -6,6 +6,26 @@ using namespace std;
 #define all(v) v.begin(),v.end()
 
 
+/*
+Fast and robust Hilbert curve order finder for Mo's Algorithm
+To use it just add 'order' to query struct and sort based on it
+*/
+ll gilbertOrder(int x, int y, int pow = 19, int rotate = 0) {
+    if (pow == 0) return 0;
+    int hpow = 1 << (pow - 1);
+    int seg = (x < hpow) ? ((y < hpow) ? 0 : 3) : ((y < hpow) ? 1 : 2);
+    seg = (seg + rotate) & 3;
+    const int rotateDelta[4] = {3, 0, 0, 1};
+    int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+    int nrot = (rotate + rotateDelta[seg]) & 3;
+    ll subSquareSize = 1LL << (2 * pow - 2);
+    ll ans = seg * subSquareSize;
+    ll add = gilbertOrder(nx, ny, pow - 1, nrot);
+    ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
+    return ans;
+}
+
+
 int sqrtN;
 class MoAlgorithm {
 public:
